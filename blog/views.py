5feafb90fb03a198fd1ddca50f3d75fe38d00dc8
Nikita Volokitin <n.a.volokitin@gmail.com>
2024-02-13 23:08:58 +0500

    Fix

--- blog/views.py
+++ blog/views.py
@@ -52,21 +52,14 @@ def post_new(request):
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
-        form = PostForm(
-            request.POST,
-            isinstance=post
-        )
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('blog:post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(
-        request,
-        'blog/post_edit.html',
-        {'form': form}
-    )
+    return render(request, 'blog/post_edit.html', {'form': form})
